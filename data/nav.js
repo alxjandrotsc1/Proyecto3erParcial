5c7e2a7ccabe8eb1030c09ca96d278a5e9245284
alxjandrotsc1 <jalejandro.torresc01@outlook.com>
2025-07-29 02:03:16 -0600

    upd commit2

--- data/nav.js
+++ data/nav.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   const toggleMenu = document.getElementById('toggleMenu');
   const navMenu = document.getElementById('navMenu');
+
+  if (!toggleMenu || !navMenu) {
+    console.error('No se encontraron elementos toggleMenu o navMenu');
+    return;
+  }
+
   const navLinks = navMenu.querySelectorAll('a');
 
   toggleMenu.addEventListener('click', () => {
